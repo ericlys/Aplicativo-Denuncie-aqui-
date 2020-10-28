3e201d516a67ecab2222b4540a6d92febd260b23
Ericlys Moreira <ericlysm@gmail.com>
2020-10-28 20:48:24 -0300

    envio de email e ativacao de conta

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import Mail from '../lib/Mail';
 import CreateUserService from '../services/CreateUserService';
+import ActiveUserService from '../services/ActiveUserService';
 
 const usersRouters = Router();
 
@@ -21,12 +22,23 @@ usersRouters.post('/', async (request, response) => {
     await Mail.sendMail({
       to: user.email,
       subject: 'Confirmação de conta',
-      html: `<a href="http://localhost:3333/confirm?user=${user.id}">Clique aqui para confirmar sua conta</a>`,
+      html: `<a href="<a href="http://localhost:3333/users/activation/${user.id}">Clique aqui para confirmar sua conta</a>`,
     });
 
     delete user.password;
 
     return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+usersRouters.get('/activation/:token', async (request, response) => {
+  try {
+    const { token } = request.params;
+    const activeUser = new ActiveUserService();
+    activeUser.execute(token);
+    return response.status(200).send('usuario ativado');
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
---
+++ server/src/services/ActiveUserService.ts
@@ -0,0 +1,22 @@
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+
+class CreateUserService {
+  public async execute(token: string): Promise<boolean> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { id: token },
+    });
+
+    if (!checkUserExists) {
+      throw new Error('Invalid token.');
+    }
+
+    await usersRepository.update({ id: token }, { checked: true });
+
+    return true;
+  }
+}
+
+export default CreateUserService;
