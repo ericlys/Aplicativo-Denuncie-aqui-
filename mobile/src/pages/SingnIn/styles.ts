3c863660c2af42566b4e2e0fc9a2ff4ef6e4cf59
Ericlys Moreira <ericlysm@gmail.com>
2020-11-01 18:07:25 -0300

    Tela de cadastro

--- mobile/src/pages/SingnIn/styles.ts
+++ mobile/src/pages/SingnIn/styles.ts
@@ -13,3 +13,35 @@ export const Title = styled.Text`
   font-family: 'RobotoSlab-Medium';
   margin: 60px 0 24px;
 `;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #000;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #ffffff;
+  border-top-width: 1px;
+  border-color: #e5e5e5;
+  padding: 16px 0;
+
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #bb1818;
+  font-size: 18px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
+`;
