80873705c5118df2a007dfe70eeaed3cb3028923
Ericlys Moreira <ericlysm@gmail.com>
2020-11-03 15:14:48 -0300

    Cadastro na aplicacao

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -13,6 +13,7 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErros';
 
@@ -66,6 +67,16 @@ const SignUp: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
+
+      const a = await api.post('/users', data);
+      console.log(a);
+      console.log(data);
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'entre no seu email para confirmar e poder faser login na aplicação!',
+      );
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
