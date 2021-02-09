6f1723f99400cbc2e2ca31977c4fb36de90f726a
Ericlys Moreira <ericlysm@gmail.com>
2021-02-09 15:27:02 -0300

    configurando mail

--- server/src/config/mail.ts
+++ server/src/config/mail.ts
@@ -3,8 +3,8 @@ export default {
   port: 2525,
   secure: false,
   auth: {
-    user: '242531557035e3',
-    pass: '9cd510652eba83',
+    user: '4bf4c121cecd68',
+    pass: '8c8ef3335ebfd0',
   },
   default: {
     from: 'Projeto tcc <noreply@tcc.com>',
