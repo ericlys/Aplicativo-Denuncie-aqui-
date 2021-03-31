930053ce1b8c790117c474fd89625f505d2bc503
Ericlys Moreira <ericlysm@gmail.com>
2021-03-30 21:58:01 -0300

    inserir usuario anonimo, apos criacao das tabelas

--- server/src/shared/infra/typeorm/migrations/1613789718575-AddAvatarFieldToUsers.ts
+++ server/src/shared/infra/typeorm/migrations/1613789718575-AddAvatarFieldToUsers.ts
@@ -8,9 +8,34 @@ export default class AddAvatarFieldToUsers1613789718575
     await queryRunner.query(
       `ALTER TABLE "users" ADD "avatar" character varying`,
     );
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into('users')
+      .values([
+        {
+          id: `${process.env.UUID_ANONYMOUS_USER}`,
+          name: 'anonymous',
+          email: 'anonymous@anonymous',
+          cpf: '000.000.000-00',
+          password: 'anonymous',
+          avatar: null,
+        },
+      ])
+      .execute();
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "avatar"`);
+    await queryRunner.manager
+      .createQueryBuilder()
+      .delete()
+      .from('users')
+      .where([
+        {
+          id: `${process.env.UUID_ANONYMOUS_USER}`,
+        },
+      ])
+      .execute();
   }
 }
