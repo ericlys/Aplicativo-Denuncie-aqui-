d0a8260bf366d104f9bba41f3d5e0c1c07571f6c
Ericlys Moreira <ericlysm@gmail.com>
2021-03-31 00:35:21 -0300

    inserir usuario padrao ao criar tabela, pois sera usado como fk para usuarios anonimos

--- server/src/shared/infra/typeorm/migrations/1603850989423-RelationDenunciationAddress.ts
+++ server/src/shared/infra/typeorm/migrations/1603850989423-RelationDenunciationAddress.ts
@@ -10,13 +10,13 @@ export default class RelationDenunciationAddress1603850989423
       `ALTER TABLE "denunciations" ADD CONSTRAINT "UQ_7ebf4b5e4fa34d3658f9d9cbfaa"  UNIQUE ("addressId")`,
     );
     await queryRunner.query(
-      `ALTER TABLE "denunciations" ADD CONSTRAINT "FK_7ebf4b5e4fa34d3658f9d9cbfaa" FOREIGN KEY ("addressId") REFERENCES "addresses"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+      `ALTER TABLE "denunciations" ADD CONSTRAINT "FK_ecc275fd65478d00d56ade92e5d" FOREIGN KEY ("addressId") REFERENCES "addresses"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "denunciations" DROP CONSTRAINT "FK_7ebf4b5e4fa34d3658f9d9cbfaa"`,
+      `ALTER TABLE "denunciations" DROP CONSTRAINT "FK_ecc275fd65478d00d56ade92e5d"`,
     );
     await queryRunner.query(
       `ALTER TABLE "denunciations" DROP CONSTRAINT "UQ_7ebf4b5e4fa34d3658f9d9cbfaa"`,
